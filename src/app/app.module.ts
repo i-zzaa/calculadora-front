61cb27827bdb74f438c866291f0cf6388f2a435a
Andressa Novaes <isa.desenvolvimento@gmail.com>
2022-06-27 20:53:55 -0300

    rm node_modules e ajuste de layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { LoginComponent } from './_component/login/login.component';
-import { UserComponent } from './_component/pages/user/user.component';
-import { TableComponent } from './_component/pages/table/table.component';
-import { DashboardComponent } from './_component/dashboard/dashboard.component';
-import { ChequeEmpresarialComponent } from './_component/pages/cheque-empresarial/cheque-empresarial.component';
-import { ParceladoPreComponent } from './_component/pages/parcelado-pre/parcelado-pre.component';'./_component/pages/parcelado-pre/parcelado-pre.component';
-import { AdminLayoutComponent } from './_component/layouts/admin-layout/admin-layout.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { LoginComponent } from "./_component/login/login.component";
+import { UserComponent } from "./_component/pages/user/user.component";
+import { TableComponent } from "./_component/pages/table/table.component";
+import { ChequeEmpresarialComponent } from "./_component/pages/cheque-empresarial/cheque-empresarial.component";
+import { ParceladoPreComponent } from "./_component/pages/parcelado-pre/parcelado-pre.component";
+("./_component/pages/parcelado-pre/parcelado-pre.component");
+import { AdminLayoutComponent } from "./_component/layouts/admin-layout/admin-layout.component";
 
-import { SidebarModule } from './_component/sidebar/sidebar.module';
-import { NavbarModule} from './_component/navbar/navbar.module';
+import { SidebarModule } from "./_component/sidebar/sidebar.module";
+import { NavbarModule } from "./_component/navbar/navbar.module";
 import { ToastrModule } from "ngx-toastr";
 
-import { DataTablesModule } from 'angular-datatables';
-import { IndicesComponent } from './_component/pages/indices/indices.component';
-import { LogComponent } from './_component/pages/log/log.component';
-import { PesquisaComponent } from './_component/pages/pesquisa/pesquisa.component';
-import { SimulacaoComponent } from './_component/pages/simulacao/simulacao.component';
-import { NotificacaoComponent } from './_component/pages/notificacao/notificacao.component';
-import { ParcelasComponent } from './_component/pages/parcelas/parcelas.component';
-import { AmortizacaoComponent } from './_component/pages/amortizacao/amortizacao.component';
-import { CallbackPipe } from './_pipe/callback.pipe';
-import { VincendasDirective } from './_directive/vincendas.directive';
-import { FooterDirective } from './_directive/footer.directive';
-import { NgSelectModule } from '@ng-select/ng-select';
-import { ParcelaPagaDirective } from './_directive/parcela-paga.directive';
+import { DataTablesModule } from "angular-datatables";
+import { IndicesComponent } from "./_component/pages/indices/indices.component";
+import { LogComponent } from "./_component/pages/log/log.component";
+import { PesquisaComponent } from "./_component/pages/pesquisa/pesquisa.component";
+import { SimulacaoComponent } from "./_component/pages/simulacao/simulacao.component";
+import { NotificacaoComponent } from "./_component/pages/notificacao/notificacao.component";
+import { ParcelasComponent } from "./_component/pages/parcelas/parcelas.component";
+import { AmortizacaoComponent } from "./_component/pages/amortizacao/amortizacao.component";
+import { CallbackPipe } from "./_pipe/callback.pipe";
+import { VincendasDirective } from "./_directive/vincendas.directive";
+import { FooterDirective } from "./_directive/footer.directive";
+import { NgSelectModule } from "@ng-select/ng-select";
+import { ParcelaPagaDirective } from "./_directive/parcela-paga.directive";
 
 @NgModule({
   declarations: [
@@ -38,7 +38,6 @@ import { ParcelaPagaDirective } from './_directive/parcela-paga.directive';
     LoginComponent,
     UserComponent,
     TableComponent,
-    DashboardComponent,
     AdminLayoutComponent,
     ChequeEmpresarialComponent,
     ParceladoPreComponent,
@@ -52,7 +51,7 @@ import { ParcelaPagaDirective } from './_directive/parcela-paga.directive';
     CallbackPipe,
     VincendasDirective,
     FooterDirective,
-    ParcelaPagaDirective
+    ParcelaPagaDirective,
   ],
   imports: [
     BrowserModule,
@@ -64,9 +63,9 @@ import { ParcelaPagaDirective } from './_directive/parcela-paga.directive';
     NavbarModule,
     ToastrModule.forRoot(),
     DataTablesModule,
-    NgSelectModule
+    NgSelectModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
